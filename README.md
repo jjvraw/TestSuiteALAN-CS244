9458d77576fb31313ff0d4e19408ef173c1b9372
Joshua James Venter <venter.joshua@gmail.com>
2022-09-20 17:43:24 +0200

    Typechecking cases added, test.py replaced other .py files.

--- README.md
+++ README.md
@@ -1,37 +1,55 @@
 # TestSuiteALAN
 
-## How to Clone
-Go into the directory/folder which holds your alan folder.
-
-1. Then in termianl, in the directory type `clone https://github.com/jjvraw/TestSuiteALAN.git` 
+## Cloning
+1.`clone https://github.com/jjvraw/TestSuiteALAN.git` into the directory/folder which holds your alan folder.
 2. Navigate to directory `cd TestSuiteALAN`
-3. Run a script `python3 scanner.py all`
+3. Run a script `python3 test.py xxx`
 
 ---
-## For Testing Parser 
+## Testing CodeGen
+1.**Test funcdef and vardef**
+`python3 test.py funcdef`
+	- all official testcases without any errors 
+> (1) should run without any errors to continue with codegen
+
+---
+## Testing HashTable
+1. **Test HashTable**
+`python3 test.py hash`
+	- should run without any problems
+	- inserts 100,000+ random strings in hashtable
+
+---
+## Testing Type Checking 
+1. `python3 test.py typechecking official`
+	- runs through and compares official test cases
+
+---
+
+## Testing Parser 
+> Can only be used before any TypeChecking additions have been made 
 1. **Run test cases**
-`python3 parser.py`
+`python3 test.py most`
 
 2. **Run 10k generated test cases**
-`python3 parser.py tenthousand`
+`python3 test.py tenthousand`
 	- thanks Iain.
 
-3. **Run offical tests**
-`python3 parser.py offical`
+3. **Run official tests**
+`python3 test.py official`
 
 ---
 
-## For Testing Scanner
-
+## Testing Scanner
 1. **Comments**
-	`python3 scanner.py comments`
+	`python3 test.py comments`
 	- nested comments
 	- error messages; testing position
 	- curly brackets inside string literals
 	- block comments with error; testing position
 	
 2. **String Literals**
-	`python3 scanner.py strings`
+	`python3 test.py strings`
 	- escape codes 
 	- multi line string literal error
 	- illegal escape codes
@@ -40,7 +58,7 @@ Go into the directory/folder which holds your alan folder.
 	- "untidy"strings"
 	
 3. **Error handling**
-	`python3 scanner.py errors`
+	`python3 test.py errors`
 	- illegal characters
 	- numbers too long
 	- identifiers too long
@@ -49,15 +67,10 @@ Go into the directory/folder which holds your alan folder.
 	- comment not closed; normal and nested testing position
 	
 4. **All available**
-	`python3 scanner.py all`
+	`python3 test.py all`
 	- tests all above, and more.
 	
 4. **Official Test Script provided**
-	`python3 scanner.py official`
+	`python3 test.py official`
 	- unit tests provided.	
 --- 
-
-To add test cases, add xxx.alan file to appropriate folders in TestCases/ and xxx.txt to Results/
-where xxx is the same name.
-	
-	
---
+++ test.py
@@ -0,0 +1,159 @@
+import os
+from re import U
+import subprocess
+import sys
+from dataclasses import dataclass
+import difflib as dl
+
+
+class Util:
+
+    @staticmethod
+    def get_argument():
+        return sys.argv
+
+    @staticmethod
+    def get_directory(folder, argument):
+        return os.listdir("./" + "TestCases/" + folder + argument + "/")
+
+    @staticmethod
+    def make(test):
+        os.chdir("../alan/src")
+        print("--- make " + test + " " + ("-" * 63))
+        os.system("make " + test)
+        print("-" * 80)
+        os.chdir("../../TestSuiteALAN")
+
+
+class Difference:
+    results_folder = ''
+
+    def __init__(self, folder):
+        self.results_folder = folder
+
+    def display_differences(self, l):
+        for file in l:
+            s1 = file[1].splitlines()
+            print("\033[1;30;41m%s%s\033[0m" % ("DIFFERENCES IN FILE: ", file[0]))
+            with open('./Results/' + self.results_folder + '/' + file[0] + '.txt') as f:
+                s2 = f.read().splitlines()
+            for diff in dl.context_diff(s1, s2):
+                print(diff)
+
+
+class Test:
+    fails = list()
+
+    @dataclass
+    class Stats:
+        tests: int
+        passes: int
+        failures: int
+
+    CurrentResult = Stats(0, 0, 0)
+
+    def __init__(self, directory):
+        self.list = directory
+
+    def test(self, bin_file, type, arg):
+        self.list.sort()
+        for i in self.list:
+            i = i.replace(".alan", "")
+            self.out = subprocess.run(
+                ['../alan/bin/' + bin_file, './TestCases/' + type + '/' + arg + '/' + i + '.alan'],
+                capture_output=True)
+            with open('./Results/' + type + '/' + i + '.txt') as f:
+                result = f.read()
+            self.display_result(i + '.alan', result == (self.out.stdout.decode() + self.out.stderr.decode()))
+
+    def display_result(self, file, res):
+        self.CurrentResult.tests += 1
+        if res:
+            print("\033[1;30;42m%-30s %9s\033[0m" % (file, "PASS"))
+            self.CurrentResult.passes += 1
+        else:
+            print("\033[1;30;41m%-30s %9s\033[0m" % (file, "FAIL"))
+            self.fails.insert(self.CurrentResult.failures - 1,
+                              [file.replace(".alan", ""), self.out.stdout.decode() + self.out.stderr.decode()])
+            self.CurrentResult.failures += 1
+
+    def display_stats(self):
+        print("%i TESTS: %i PASSES and %i FAILS" % (
+            self.CurrentResult.tests, self.CurrentResult.passes, self.CurrentResult.failures))
+        print(str(round(self.CurrentResult.passes / self.CurrentResult.tests, 4) * 100) + '%')
+
+    @staticmethod
+    def hash(bin_file, file):
+        with open(file) as f:
+            country = f.readline()
+            out = subprocess.run(['../alan/bin/' + bin_file, 'co', 'to'])
+
+
+
+if __name__ == '__main__':
+
+    argument = Util.get_argument()
+    # Get arguments
+    if len(argument) == 3:
+        arg = argument[1] + " " + argument[2]
+
+    # Set directory and testxxx
+    test = ''
+    directory = list()
+    loc = ''
+    diff = ''
+    bin_file = ''
+
+    if argument[1] == 'scanner':
+        directory = Util.get_directory('scanner/', argument[2])
+        test = 'testscanner'
+        diff = 'scanner'
+        bin_file = 'testscanner'
+
+    elif argument[1] == 'parser':
+        test = 'testparser'
+        diff = 'parser'
+        bin_file = 'alanc'
+
+        if argument[2] == 'most':
+            directory = Util.get_directory('parser/', '')
+            directory.remove("tenthousand")
+            directory.remove("official")
+
+        else:
+            directory = Util.get_directory('parser/', argument[2])
+
+    elif argument[1] == 'typechecking':
+        directory = Util.get_directory('typechecking/', argument[2])
+        test = 'testtypechecking'
+        bin_file = 'alanc'
+        diff = 'typechecking'
+
+    elif argument[1] == 'hash': 
+        Util.make('testhashtable')
+        os.system('./hash')
+
+    elif argument[1] == 'codegen': 
+        directory = Util.get_directory('codegen/', argument[2])
+        test = 'testtypechecking'
+        bin_file = 'alanc'
+        diff = 'codegen'
+
+
+    if argument[1] != 'hash':
+        Util.make(test)
+        test = Test(directory)
+        test.test(bin_file, argument[1], argument[2])
+        test.display_stats()
+
+        print("-" * 80)
+        print("Enter 0 to exit, or 1 to display differences in failed cases: ")
+
+        for line in sys.stdin:
+            if line.rstrip() == "1":
+                diffy = Difference(diff)
+                diffy.display_differences(test.fails)
+                break
+            if line.rstrip() == "0":
+                break
+
